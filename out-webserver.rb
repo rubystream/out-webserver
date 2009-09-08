e7cac623cb75642c78922f2c6f321d249b5a7304
Dejan Dimic <dejan.dimic@gmail.com>
2009-09-09 00:32:38 +0200

    Add new, edit, update, delete action for Users. Add initial views for new and edit.

--- out-webserver.rb
+++ out-webserver.rb
@@ -31,18 +31,127 @@ get '/' do
   "This is a initial test!"
 end
 
-get '/users' do
+# list all users
+get '/users/?' do
   content_type 'application/xml', :charset => 'utf-8'
 
   @users = User.find(:all)
 
   xml = Builder::XmlMarkup.new :indent => 2
   xml.instruct!
-  xml.node(:name=>"http://www.steirischerherbst.at",
-           :description=>"http://www.steirischerherbst.at") do |node|
-    @users.each do |user|
-      xml.node(:name => "#{user.lastname} #{user.firstname}", :email=>"#{user.email}")
-    end unless @users.nil?
+  xml.result(:code=>"200", :description =>"OK") do |result|
+    result.node(:id => "0", 
+                :name=>"http://www.steirischerherbst.at",
+                :description=>"http://www.steirischerherbst.at") do |node|
+      @users.each do |user|
+        xml.node(:id => "#{user.id}",
+                 :name => "#{user.lastname} #{user.firstname}", 
+                 :email=>"#{user.email}")
+      end unless @users.nil?
+    end
   end
+end
 
+# Show user
+get '/users/:id' do
+  content_type 'application/xml', :charset => 'utf-8'
+
+  xml = Builder::XmlMarkup.new :indent => 2
+  xml.instruct!
+
+  begin
+    @user = User.find(params[:id])
+    xml.node(:id => "#{user.id}",
+             :name => "#{user.lastname} #{user.firstname}", 
+             :email => "#{user.email}")
+  rescue ActiveRecord::RecordNotFound => err
+    xml.result(:code=>"404", :description=> "Record Not found")
+    throw :halt, [404, xml.target!]
+  end
+end
+
+# Create user
+post '/users/?' do
+  content_type 'application/xml', :charset => 'utf-8'
+
+  xml = Builder::XmlMarkup.new :indent => 2
+  xml.instruct!
+
+  @user = User.new(params[:user])
+  if @user.save
+    xml.result(:code=>"200", :description =>"OK") do |result|
+      result.node(:id => "#{user.id}", 
+                  :name => "#{user.lastname} #{user.firstname}", 
+                  :email=>"#{user.email}")
+    end
+  else
+    xml.result(:code => "400", :description => "Bad Request") do |result|
+      result.errors do |item|
+        @user.errors.each do |field, msg|
+          item.error(:field => "#{field}", :message => "#{msg}")
+        end
+      end
+    end
+  end
+end
+
+# Update user
+put '/users/:id' do
+  content_type 'application/xml', :charset => 'utf-8'
+
+  xml = Builder::XmlMarkup.new :indent => 2
+  xml.instruct!
+
+  begin
+    @user.find(params[:id])
+
+    if (@user.update_attributes(params[:user]))
+        xml.result(:code=>"200", :description =>"OK") do |result|
+          result.node(:id => "#{user.id}",
+                      :name => "#{user.lastname} #{user.firstname}",
+                      :email => "#{user.email}")
+        end
+    else
+      xml.result(:code => "400", :description => "Bad Request") do |result|
+        result.errors do |item|
+          @user.errors.each do |field, msg|
+            item.error(:field => "#{field}", :message => "#{msg}")
+          end
+        end
+      end
+    end
+  rescue ActiveRecord::RecordNotFound
+    xml.result(:code=>"404", :description=> "Record Not found")
+    throw :halt, [404, xml.target!]
+  end
+end
+
+# Destroy user
+delete '/users/:id' do
+  content_type 'application/xml', :charset => 'utf-8'
+
+  xml = Builder::XmlMarkup.new :indent => 2
+  xml.instruct!
+
+  begin
+    @user.find(params[:id])
+    @user.destroy
+
+    xml.result(:code=>"200", :description =>"OK") 
+  rescue ActiveRecord::RecordNotFound
+    xml.result(:code=>"404", :description=> "Record Not found")
+    throw :halt, [404, xml.target!]
+  end
+end
+
+# New user
+get '/users/new/?' do
+  @user = User.new
+  erb :new
+end
+
+# Edit user
+get '/users/:id/edit/?' do
+  @user = User.find(params[:id])
+  erb :edit
 end
