05cba28eb12f4f5d244c0d824f0c2143e0fe3450
Dejan Dimic <dejan.dimic@gmail.com>
2009-09-10 15:59:11 +0200

    Add initially picture as attachemnt (using paperclip) to User model.

--- db/models/User.rb
+++ db/models/User.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+  has_attached_file :picture,
+    :styles => { :thumb => "120x120", :medium => "550x550" }
+
   validates_presence_of :firstname, :lastname, :email
   validates_uniqueness_of :email
 end
